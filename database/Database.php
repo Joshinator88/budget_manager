0511a97ddc0ec0fd02e722253af1a8d64fcae380
Joshua Bruijn <joshuaraatig@hotmail.com>
2023-09-29 18:34:22 +0200

    added gitignore

--- database/Database.php
+++ database/Database.php
@@ -1,43 +1,38 @@
 <?php
 
-class Database {
-    
+// hai
 
+class Database
+{
     public $connection;
     private $_host, $_username, $_password, $_database;
 
     // method to create a database connection
-    public function connect() {
+    public function connect()
+    {
 
         $iniInfo = parse_ini_file('config.ini');
             
-        if (!empty($iniInfo) && $iniInfo !== NULL) {
-            
+        if (!empty($iniInfo) && $iniInfo !== null) {
             try {
-            
                 $this->DatabaseLogin($iniInfo);
                 
                 $this->connection = new PDO("mysql:host=$this->_host;dbname=$this->_database", $this->_username, $this->_password);
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
                 echo "connected successfully";
-    
             } catch (PDOException $e) {
                 echo "Connection failed " . $e->getMessage();
             }
-            
         }
-
-        
     }
 
 
-    private function DatabaseLogin($detailfile) {
+    private function DatabaseLogin($detailfile)
+    {
         $this->_host = $detailfile['host'];
         $this->_database = $detailfile['database'];
         $this->_username = $detailfile['username'];
         $this->_password = $detailfile['password'];
     }
-
-
 }
